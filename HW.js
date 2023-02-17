9133c7466b6a672e47e90e7f6ec7e5063f40f3f1
Denysssssss12 <112236035+Denysssssss12@users.noreply.github.com>
2023-02-17 21:10:51 +0200

    Summary

---
+++ HW.js
@@ -0,0 +1,41 @@
+// 1
+// const hello = function(){
+//     return"Привіт JavaScript";
+// }
+//     console.log(hello());
+
+
+// 2
+
+// function hello(name){
+//     consle.log(`Hello${name}`);
+// }
+// hello("Василь");
+
+
+// 3
+// const nul = function(n, n){
+//     console.log(n + n)
+//     console.log(n * n)
+//     console.log(n - n)
+// }
+// nul(345,564);
+
+
+// 4
+
+// const myNewArrowFunction =( ...rest) =>{
+//     console.log(rest);
+// }
+
+// 5
+
+// const myAvergScore = (...args) => {
+//     console.log(args)
+//     let totalScore = 0;
+//     for(const element of args){
+//         totalScore += element
+//     }
+//     totalScore = totalScore / args.length
+// }
+
